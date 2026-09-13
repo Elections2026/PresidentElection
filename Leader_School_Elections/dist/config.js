a98835cd23be00c301b56b3ac1ded77cc8aa263b
Elections2026 <tsaa.uz.official@gmail.com>
2026-09-13 17:00:52 +0500

    Update election opening and closing dates

--- Leader_School_Elections/dist/config.js
+++ Leader_School_Elections/dist/config.js
@@ -1,8 +1,8 @@
 window.ELECTION_CONFIG = {
   schoolName: 'Leader School',
   totalVoters: 423,
-opensAt: '2026-09-18T09:00:00+05:00',
-closesAt: '2026-09-19T09:00:00+05:00',
+opensAt: '2026-09-17T09:00:00+05:00',
+closesAt: '2026-09-18T09:00:00+05:00',
   timeZone: 'Asia/Tashkent',
   formUrl: 'https://forms.gle/E8LXjWM4ZaHcdfBH9',
   // ЯВКА: после проверки меняйте только эти две строки. Процент считается сам.
